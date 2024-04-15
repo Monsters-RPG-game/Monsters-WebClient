ce4a284477a1d52a626df24fcca9f945fbdfefc5
Jakub <kiszczyckuba20@wp.pl>
2024-04-15 16:55:29 +0200

    Fix: Added websocket pinging

--- src/controllers/websocket.ts
+++ src/controllers/websocket.ts
@@ -1,12 +1,34 @@
 import type { ISocketMessage, ISocketNewMessage } from '../types';
 import { ESocketType } from '../enums';
 import { saveLog } from '../communication';
+import { sleep } from '../tools';
 
 export default class Controller {
-  private readonly _add: (target: string, message: string) => Promise<void>;
+  private readonly _add: (target: string, message: string, send?: boolean) => Promise<void>;
 
-  constructor(add: (target: string, message: string) => void) {
-    this._add = (target: string, command: string): Promise<void> => this.prepareAdd(add, target, command);
+  constructor(add: (target: string, message: string, notSend?: boolean) => void) {
+    this._add = (target: string, command: string, notSend?: boolean): Promise<void> =>
+      this.prepareAdd(add, target, command, notSend);
+  }
+
+  private _heartbeat: NodeJS.Timeout | undefined;
+
+  private get heartbeat(): NodeJS.Timeout {
+    return this._heartbeat as NodeJS.Timeout;
+  }
+
+  private set heartbeat(value: NodeJS.Timeout) {
+    this._heartbeat = value;
+  }
+
+  private _retries: number = 0;
+
+  private get retries(): number {
+    return this._retries;
+  }
+
+  private set retries(value: number) {
+    this._retries = value;
   }
 
   private _client: WebSocket | undefined;
@@ -19,7 +41,7 @@ export default class Controller {
     this._client = value;
   }
 
-  private get add(): (target: string, message: string) => Promise<void> {
+  private get add(): (target: string, message: string, notSend?: boolean) => Promise<void> {
     return this._add;
   }
 
@@ -28,9 +50,10 @@ export default class Controller {
     this.client = new WebSocket(server);
 
     this.startListeners();
+    this.initHeartbeat();
   }
 
-  close(reason?: string): void {
+  async close(reason?: string): Promise<void> {
     if (!this._client) return;
     this._client.close();
     this._client = undefined;
@@ -42,8 +65,57 @@ export default class Controller {
       } catch (err) {
         // ignored
       }
+
+      await this.add('System', 'Connection with server closed. Retrying', true);
       console.log('Socket closed');
       console.log(r);
+
+      await this.reconnect();
+    }
+  }
+
+  private async reconnect(): Promise<void> {
+    console.log('reconnecting');
+    if (this.client.readyState !== 3) {
+      await this.add('System', 'Server reconnected', true);
+      return;
+    }
+    if (this.retries === 10) {
+      await this.add(
+        'System',
+        'Could not reconnect to server after 10 retries. Connection that died is used to handle live updates and messages. Try to refresh this page after 2-3m',
+        true,
+      );
+      return;
+    }
+
+    const server = import.meta.env.VITE_API_WS_BACKEND as string;
+    this.client = new WebSocket(server);
+    this.retries++;
+    await sleep(5000);
+    await this.reconnect();
+  }
+
+  private initHeartbeat(): void {
+    this.heartbeat = setInterval(() => {
+      this.ping();
+    }, 5000);
+  }
+
+  private ping(): void {
+    if (this.retries === 10) {
+      this.add(
+        'System',
+        'Server disconnected. Connection that got destroyed is used to handle live updates and messages. Try to refresh this page after 2-3m',
+        true,
+      ).catch((err) => {
+        console.log("Couldn't add data");
+        console.log(err);
+      });
+      clearInterval(this.heartbeat);
+    } else {
+      this.retries++;
+      this.client?.send('ping');
     }
   }
 
@@ -59,7 +131,7 @@ export default class Controller {
         console.log('Got error with websocket message', err);
       });
     };
-    this.client.onclose = (e): void => this.close(e.reason);
+    this.client.onclose = async (e): Promise<void> => this.close(e.reason);
   }
 
   private async handleMessage(msg: string): Promise<void> {
@@ -93,8 +165,11 @@ export default class Controller {
     add: (target: string, command: string) => void,
     target: string,
     input: string,
+    notSend?: boolean,
   ): Promise<void> {
     add(target, input);
-    await saveLog(target, input);
+    if (!notSend) {
+      await saveLog(target, input);
+    }
   }
 }
--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,7 +1,7 @@
 import Cookies from './cookies';
 import getHttpClient from './axios';
 
-const generateRandomName = (max = 20): string => {
+export const generateRandomName = (max = 20): string => {
   const vocabulary = 'ABCDEFGHIJKLMNOUPRSTUWZabcdefghijklmnouprstuwz';
   let name = '';
   for (let x = 0; x < max; x++) {
@@ -10,4 +10,13 @@ const generateRandomName = (max = 20): string => {
   return name;
 };
 
-export { getHttpClient, Cookies, generateRandomName };
+export const sleep = async (milliseconds: number): Promise<void> => {
+  // eslint-disable-next-line compat/compat
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, milliseconds);
+  });
+};
+
+export { getHttpClient, Cookies };
