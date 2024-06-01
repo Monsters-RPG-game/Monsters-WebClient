24889ee502f9b405bd9ce81e26bdf369856f8660
Jakub <kiszczyckuba20@wp.pl>
2024-06-01 18:00:49 +0200

    Fix: Added fix for messages beeing send too fast and for user authorization for different origins

--- src/controllers/websocket.ts
+++ src/controllers/websocket.ts
@@ -1,8 +1,11 @@
 import type { ISocketMessage, ISocketOutMessage, ISocketNewMessage } from '../types';
-import { ESocketType } from '../enums';
+import { ESocketType, ETokenNames } from '../enums';
+import { Cookies } from '../tools';
 
 export default class Controller {
   private readonly _add: (target: string, message: string) => Promise<void>;
+
+  private _resolve: { action: ((val: void | PromiseLike<void>) => void), timer: NodeJS.Timeout | null } | undefined = undefined;
 
   constructor(add: (target: string, message: string) => void) {
     this._add = (target: string, command: string): Promise<void> => this.prepareAdd(add, target, command);
@@ -11,6 +14,14 @@ export default class Controller {
   private _client: WebSocket | undefined;
 
   private _request: ((value: Promise<ISocketMessage> | ISocketMessage) => void) | undefined = undefined;
+
+  private get resolve(): { action: (val: void | PromiseLike<void>) => void, timer: NodeJS.Timeout | null } {
+    return this._resolve as { action: (val: void | PromiseLike<void>) => void, timer: NodeJS.Timeout | null };
+  }
+
+  private set resolve(val: { action: (val: void | PromiseLike<void>) => void, timer: NodeJS.Timeout | null }) {
+    this._resolve = val;
+  }
 
   private get client(): WebSocket {
     return this._client as WebSocket;
@@ -38,7 +49,8 @@ export default class Controller {
     return new Promise(resolve => {
       const server = import.meta.env.VITE_API_WS_BACKEND as string;
       this.client = new WebSocket(server);
-      this.startListeners(resolve);
+      this.resolve = { action: resolve, timer: null };
+      this.startListeners();
     });
   }
 
@@ -67,9 +79,11 @@ export default class Controller {
     });
   }
 
-  private startListeners(resolve: (val: void | PromiseLike<void>) => void): void {
+  private startListeners(): void {
     this.client.onopen = (): void => {
-      resolve();
+      this.resolve.timer = setTimeout(() => {
+        this.resolve.action();
+      }, 2000);
       console.log('websocket connected');
     };
     this.client.onerror = (err): void => {
@@ -102,10 +116,29 @@ export default class Controller {
       case ESocketType.Message:
         await this.handleUserMessage(parsed.payload as ISocketNewMessage);
         break;
+      case ESocketType.Error:
+        await this.handleError(parsed.payload as Error);
+        break;
       default:
         console.log('Unknown websocket message');
         console.log(parsed);
         break;
+    }
+  }
+
+  private async handleError(error: Error): Promise<void> {
+    if (error.name === 'AwaitingAuthorizationError') {
+      const cookies = new Cookies();
+      const access = cookies.getToken(ETokenNames.Access);
+      await this.send({
+        target: 'authorization',
+        subTarget: '',
+        payload: {
+          key: access
+        }
+      });
+    } else {
+      console.log('Got error from server', error);
     }
   }
 
@@ -126,3 +159,4 @@ export default class Controller {
     });
   }
 }
+
