96d01b982ca3371ca2d670f237f9c8e2345d10f9
HurasAdam <huradam@wp.pl>
2024-06-02 20:29:53 +0200

    Feat: Added CombatStage view component.

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IoCloseOutline } from 'react-icons/io5';
+import images from '../constants/images';
 
 interface IProps{
     children:boolean;
@@ -8,20 +8,29 @@ interface IProps{
 
 const Popup:React.FC = ({children,setOrderPopup}) => {
 
+    const BannerImg = {
+        backgroundImage: `url(${images.CombatSceneImage})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        height: "100%",
+        width: "100%",
+      };
+
   return (
     <>
 
 {  (
-    <div className='popup'>
+    <div className='popup  ' >
 
-<div className='h-screen w-screen fixed top-[100px] left-0 bg-black/40 z-50 backdrop-blur-sm'>
+<div  className='h-[100%]  w-screen fixed   left-0 bg-black/40 z-50 backdrop-blur-sm'>
 
-<div className='fixed top-0 left-0   p-4 shadow-md bg-white dark:bg-gray-900 rouded-md duration-200 w-full h-full'>
+<div style={BannerImg} className='fixed  top-0 left-0   p-4 shadow-md bg-white dark:bg-gray-900 rouded-md duration-200 w-full h-full'>
 
     {/* Header section */}
     <div className='flex items-center justify-between' />
     {/* Form section */}
-    <div className='mt-4 '>
+    <div className=' h-full  '>
 
 
     {children}
