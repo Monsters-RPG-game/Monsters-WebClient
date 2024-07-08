1317df1cd5c4d8a944693f29a66ee8ff4762b3df
DominikDO98 <dominiklas13@wp.pl>
2024-07-08 22:40:18 +0200

    Fix: Removed error message logic, reaarange props, added AlertDialogTitle, chenged AlertDiaglogCancel logic

--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -1,28 +1,19 @@
-import { useState } from 'react';
 import type { IPortalProps } from '../types/portal';
 import * as dialogs from './ui/alert-dialog';
 
-const Portal: React.FC<IPortalProps> = ({ className, children, openButton, handleClose, isPortalOpen }) => {
-  const [errorMsg, setErrorMsg] = useState('');
-
+const Portal: React.FC<IPortalProps> = ({ title, className, children, open, setOpen, openButton }) => {
   return (
-    <dialogs.AlertDialog open={isPortalOpen}>
-      <dialogs.AlertDialogTrigger>{openButton}</dialogs.AlertDialogTrigger>
+    <dialogs.AlertDialog open={open} onOpenChange={setOpen}>
+      {openButton && <dialogs.AlertDialogTrigger>{openButton}</dialogs.AlertDialogTrigger>}
       <dialogs.AlertDialogContent className={className}>
-        <div className="h-[30%]">
-          {children}
-          {errorMsg && (
-            <div className=" absolute top-1/2  w-full flex flex-col items-center ">
-              <span className="text-2xl text-slate-300  ">{errorMsg}</span>
-              <span className="text-base text-slate-400 ">please try again later</span>
-            </div>
-          )}
-        </div>
+        <dialogs.AlertDialogTitle>
+          <div className="text-slate-600 font-heroSectionFont text-3xl font-bold tracking-tight">{title}</div>
+        </dialogs.AlertDialogTitle>
+        <div className="h-[100%]">{children}</div>
         <dialogs.AlertDialogCancel
           className=" absolute top-0 right-0 bg-transparent text-slate-400 my-2 border-none hover:bg-transparent hover:text-slate-300"
           onClick={() => {
-            handleClose();
-            setErrorMsg('');
+            setOpen(false);
           }}
         >
           &#x2716;
