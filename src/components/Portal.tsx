2db077fdeaf3a8e1560fdafc7aaef650611e95b6
DominikDO98 <dominiklas13@wp.pl>
2024-07-08 20:53:03 +0200

    Fix: Removed leftovers from Portal component: TerminalPortal, DeleteAccountForm component, confitmDialog state and confirmDeleteAccountHandler function

--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -2,36 +2,14 @@ import { useState } from 'react';
 import type { IPortalProps } from '../types/portal';
 import * as dialogs from './ui/alert-dialog';
 
-// const confirmDeleteAccountHandler = async (
-//   value: boolean,
-//   cb: () => Promise<void> | Promise<AxiosResponse>,
-//   setErrorMsg: React.Dispatch<React.SetStateAction<string>>,
-//   setConfirmDialog: React.Dispatch<React.SetStateAction<boolean>>,
-// ): Promise<void> => {
-//   try {
-//     await cb();
-//     setErrorMsg('');
-//   } catch (error) {
-//     console.log('Error', error);
-//     setErrorMsg(`looks like ${(error as Error)?.message}`);
-
-//     setTimeout(() => {
-//       setErrorMsg('');
-//     }, 4500);
-//     return;
-//   }
-//   setConfirmDialog(value);
-// };
-
 const Portal: React.FC<IPortalProps> = ({ className, children, openButton, handleClose, isPortalOpen }) => {
-  const [confirmDialog, setConfirmDialog] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
 
   return (
     <dialogs.AlertDialog open={isPortalOpen}>
       <dialogs.AlertDialogTrigger>{openButton}</dialogs.AlertDialogTrigger>
       <dialogs.AlertDialogContent className={className}>
-        <div className={confirmDialog ? 'h-[30%]' : 'h-[100%] relative '}>
+        <div className="h-[30%]">
           {children}
           {errorMsg && (
             <div className=" absolute top-1/2  w-full flex flex-col items-center ">
@@ -44,25 +22,11 @@ const Portal: React.FC<IPortalProps> = ({ className, children, openButton, handl
           className=" absolute top-0 right-0 bg-transparent text-slate-400 my-2 border-none hover:bg-transparent hover:text-slate-300"
           onClick={() => {
             handleClose();
-            setConfirmDialog(false);
             setErrorMsg('');
           }}
         >
           &#x2716;
         </dialogs.AlertDialogCancel>
-        {/* {!confirmDialog ? (
-          <TerminalPortal
-            triggerFn={triggerFn}
-            handleClose={handleClose}
-            setTodeleteAccountHandler={setTodeleteAccountHandler}
-            confirmDeleteAccountHandler={confirmDeleteAccountHandler}
-            confirmButtonLabel={confirmButtonLabel}
-            cancelButtonLabel={cancelButtonLabel}
-            deleteButtonLabel={deleteButtonLabel}
-          />
-        ) : (
-          <DeleteAccountForm setErrorMsg={setErrorMsg} triggerFn={triggerFn} />
-        )} */}
       </dialogs.AlertDialogContent>
     </dialogs.AlertDialog>
   );
