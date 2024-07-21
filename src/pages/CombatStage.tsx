a5d676a8947a57de5b500d41f41b24dc6a231169
DominikDO98 <dominiklas13@wp.pl>
2024-07-21 20:50:43 +0200

    Fix: Created component trre for combat-stage and created target state hook

--- src/pages/CombatStage.tsx
+++ src/pages/CombatStage.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useMutation } from 'react-query';
 import { attack, leaveFight } from '../communication';
 import type { ECharacterState } from '../enums';
 import { useFightsStore, useProfileStore } from '../zustand/store';
+import { Combat } from '../components/combat/Combat';
 
 const CombatStage: React.FC = () => {
   const fights = useFightsStore((state) => state.fights);
   const playerActiveFight = useFightsStore((state) => state.activeFight);
+  const [target, setTarget] = useState<string>('');
 
   useEffect(() => {
     console.log('fights: ', fights);
     console.log('playerActiveFight: ', playerActiveFight);
-  }, [playerActiveFight]);
+    console.log('target: ', target);
+  }, [playerActiveFight, target]);
 
   const { mutate: leave } = useMutation({
     mutationFn: () => {
@@ -24,8 +27,8 @@ const CombatStage: React.FC = () => {
   });
 
   const { mutate: action } = useMutation({
-    mutationFn: (target: string) => {
-      const response = attack(target);
+    mutationFn: async (target: string) => {
+      const response = await attack(target);
       console.log('response: ', response);
       return response;
     },
@@ -37,7 +40,7 @@ const CombatStage: React.FC = () => {
   return (
     <>
       <div>CombatStage</div>
-      <div>combat</div>
+      <Combat fight={playerActiveFight} setTarget={setTarget} />
       <button
         className="flex bottom"
         type="button"
@@ -50,7 +53,7 @@ const CombatStage: React.FC = () => {
       <button
         type="button"
         onClick={() => {
-          action('target');
+          action(target);
         }}
       >
         Attack
